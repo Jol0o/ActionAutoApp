f9b47da1e565a763b107a6cf61b26eb8811c4126
roquejericoturingan <roquejericoturingan@gmail.com>
2026-02-14 05:29:17 +0800

    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from '../providers/QueryProvider'
+import "mapbox-gl/dist/mapbox-gl.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
